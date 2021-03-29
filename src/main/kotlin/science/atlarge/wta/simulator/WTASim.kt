f4a8741e1eff6108fe1263d92a8e8c74ff38b9a4
lversluis <lfdversluis@gmail.com>
2021-03-29 15:28:29 +0200

    Fix computing number of machines

--- src/main/kotlin/science/atlarge/wta/simulator/WTASim.kt
+++ src/main/kotlin/science/atlarge/wta/simulator/WTASim.kt
@@ -119,7 +119,7 @@ object WTASim {
                 val numMachines = totalResourceUsage.toBigDecimal().divide(
                     BigDecimal.valueOf(traceEndTime - traceStartTime)
                         .multiply(BigDecimal.valueOf(resourcesPerMachine[i].toLong())) // number of resources
-                        .multiply(BigDecimal.valueOf(cli.targetUtilization!!)) // target util
+                        .divide(BigDecimal.valueOf(cli.targetUtilization!!)) // target util
                         .multiply(BigDecimal.valueOf(machineFractions[i])), // Fraction of this machine
                     32, RoundingMode.CEILING
                 ).setScale(0, RoundingMode.CEILING).intValueExact()
